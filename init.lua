c2c6163b5cb5f2337b562f3d53584212b7f2af74
sefat.anam <msefat@saastaq.io>
2024-11-23 18:20:51 +0600

    enhance

--- init.lua
+++ init.lua
@@ -12,12 +12,12 @@ end
 vim.opt.rtp:prepend(lazypath)
 vim.opt.scrolloff = 10
 vim.opt.relativenumber = true -- Enable relative number
-vim.opt.number = true -- Also show the current line number
-vim.opt.expandtab = true -- Convert tabs to spaces
-vim.opt.smartindent = true -- Enable smart indentation
-vim.opt.shiftwidth = 4 -- Number of spaces for each indentation level
-vim.opt.tabstop = 4 -- Number of spaces a tab counts for
-vim.opt.softtabstop = 4 -- Number of spaces a tab key inserts
+vim.opt.number = true         -- Also show the current line number
+vim.opt.expandtab = true      -- Convert tabs to spaces
+vim.opt.smartindent = true    -- Enable smart indentation
+vim.opt.shiftwidth = 4        -- Number of spaces for each indentation level
+vim.opt.tabstop = 4           -- Number of spaces a tab counts for
+vim.opt.softtabstop = 4       -- Number of spaces a tab key inserts
 local lazy_config = require "configs.lazy"
 
 -- load plugins
@@ -28,23 +28,65 @@ require("lazy").setup({
     branch = "v2.5",
     import = "nvchad.plugins",
   },
-  { "nvchad/volt", lazy = true },
+  { "nvchad/volt",     lazy = true },
   {
     "nvchad/minty",
     cmd = { "Shades", "Huefy" },
   },
-  { "nvchad/menu", lazy = true },
+  { "nvchad/menu",     lazy = false },
   { import = "plugins" },
+  {
+    'Bekaboo/dropbar.nvim',
+    lazy = false,
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make'
+    }
+  },
+  {
+    "utilyre/barbecue.nvim",
+    lazy = false,
+    name = "barbecue",
+    version = "1.2.0",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    opts = {}
+  },
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    lazy = false,
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- optional
+      "neovim/nvim-lspconfig",         -- optional
+    },
+    opts = {}                          -- your configuration
+  },
+  {
+    -- Calls `require('slimline').setup({})`
+    "sschleemilch/slimline.nvim",
+    lazy = false,
+    opts = {}
+  },
+
 }, lazy_config)
 
+require("cmp").config.formatting = {
+  format = require("tailwindcss-colorizer-cmp").formatter
+}
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
 require "options"
 require "nvchad.autocmds"
+require('slimline').setup({})
 
 vim.schedule(function()
   require "mappings"
 end)
-
