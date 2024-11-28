4a42832472a05a027e9a7e2c599145c6128888bd
sefatanam <sefatanam@gmail.com>
2024-11-28 23:56:25 +0600

    fix lsp config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -54,9 +54,9 @@ return {
         },
         suggestion = {
           enabled = true,
-          auto_accept = true,
-          auto_trigger = true,
-          debounce = 75,
+          auto_accept = false,
+          auto_trigger = false,
+          debounce = 65,
           keymap = {
             accept = "<Tab>",
             accept_word = false,
@@ -93,11 +93,7 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     lazy = false,
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
+    opts = {},
     keys = {
       {
         "<leader>=",
@@ -142,43 +138,127 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
+    dependencies = { "neovim/nvim-lspconfig", "williamboman/mason.nvim" },
     config = function()
-      require("mason-lspconfig").setup(
-        {
-          ensure_installed = { "html", "cssls", "svelte", "angularls", "ts_ls", "eslint", "jsonls", "gopls", "rust_analyzer" },
+      local mason_lspconfig = require("mason-lspconfig")
+      local lspconfig = require("lspconfig")
+      local registry = require("mason-registry")
+      local path = require("mason-core.path")
+      local functional = require("mason-core.functional")
 
-        }
-      )
+      mason_lspconfig.setup({
+        ensure_installed = {
+          "html",
+          "cssls",
+          "svelte",
+          "angularls",
+          "ts_ls",
+          "denols",
+          "jsonls",
+          "gopls",
+          "rust_analyzer",
+          "dockerls",
+          "cmake",
+          "astro",
+          "bashls",
+          "lua_ls",
+        },
+      })
+      -- Setup LSP servers
+      mason_lspconfig.setup_handlers({
+        function(server_name) -- Default setup for all servers
+          lspconfig[server_name].setup({})
+        end,
+        ["ts_ls"] = function()
+          lspconfig.ts_ls.setup({
+            root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json"),
+          })
+        end,
+        ["denols"] = function()
+          lspconfig.denols.setup({
+            root_dir = function(fname)
+              local root = lspconfig.util.root_pattern("deno.json", "deno.jsonc")(fname)
+              if root then
+                return root
+              end
+              return nil
+            end,
+          })
+        end,
+        ["angularls"] = function()
+          -- Load Mason's registry
+          local ok, mason_registry = pcall(require, 'mason-registry')
+          if not ok then
+            vim.notify 'mason-registry could not be loaded'
+            return
+          end
+          -- Get Angular Language Server's install path from Mason
+          local angularls_path = mason_registry.get_package('angular-language-server'):get_install_path()
+          -- Define the command to start ngserver with correct paths
+          local cmd = {
+            'ngserver',
+            '--stdio',
+            '--tsProbeLocations',
+            table.concat({
+              angularls_path,
+              vim.uv.cwd(),
+            }, ','),
+            '--ngProbeLocations',
+            table.concat({
+              angularls_path .. '/node_modules/@angular/language-server',
+              vim.uv.cwd(),
+            }, ','),
+          }
+          -- LSP configuration for angularls
+          local config = {
+            cmd = cmd, -- The dynamically built command
+            on_new_config = function(new_config, new_root_dir)
+              -- Update the command with new configuration if needed
+              new_config.cmd = cmd
+            end,
+            root_dir = require('lspconfig').util.root_pattern('angular.json', '.git'), -- Detect Angular projects
+            filetypes = { 'typescript', 'html', 'typescriptreact' },                   -- File types for Angular
+            capabilities = require('nvchad.configs.lspconfig').capabilities,           -- Capabilities, like autocompletion
+            on_attach = require('nvchad.configs.lspconfig').on_attach,                 -- Attach custom LSP behaviors
+          }
+          require('lspconfig').angularls.setup(config)
+        end,
+      })
     end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
   },
   {
     "nvim-telescope/telescope-ui-select.nvim",
     lazy = false,
     -- This is your opts table
     require("telescope").setup {
+      defaults = {
+        file_ignore_patterns = {
+          "node_modules",
+          ".git",
+          ".cache",
+          ".github",
+          ".vscode",
+          ".idea",
+          ".gitignore",
+          ".gitattributes",
+          ".gitmodules",
+          ".DS_Store",
+          ".editorconfig",
+          ".eslintignore",
+          ".eslintrc",
+          ".prettierignore",
+          ".prettierrc",
+          ".stylelintrc",
+          ".stylelintignore",
+          ".huskyrc",
+          ".lintstagedrc",
+          ".browserslistrc",
+          ".babelrc",
+        },
+      },
       extensions = {
         ["ui-select"] = {
-          require("telescope.themes").get_dropdown {
-            -- even more opts
-          }
-
-          -- pseudo code / specification for writing custom displays, like the one
-          -- for "codeactions"
-          -- specific_opts = {
-          --   [kind] = {
-          --     make_indexed = function(items) -> indexed_items, width,
-          --     make_displayer = function(widths) -> displayer
-          --     make_display = function(displayer) -> function(e)
-          --     make_ordinal = function(e) -> string
-          --   },
-          --   -- for example to disable the custom builtin "codeactions" display
-          --      do the following
-          --   codeactions = false,
-          -- }
+          require("telescope.themes").get_dropdown {}
         }
       }
     }
