aff3faf910a9149f6c6be1bab47bbfd27134db2f
sefatanam <sefatanam@gmail.com>
2025-01-26 00:46:11 +0600

    remvoe unused code

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -20,24 +20,11 @@ vim.diagnostic.config({
 })
 
 
+-- Background transparent
+-- vim.cmd [[
+--   highlight Normal guibg=none
+--   highlight NonText guibg=none
+--   highlight Normal ctermbg=none
+--   highlight NonText ctermbg=none
+-- ]]
 
--- vim.opt.scrolloff = 10
--- vim.opt.relativenumber = true -- Enable relative number
--- vim.opt.number = true         -- Also show the current line number
--- vim.opt.expandtab = true      -- Convert tabs to spaces
--- vim.opt.smartindent = true    -- Enable smart indentation
--- vim.opt.shiftwidth = 4        -- Number of spaces for each indentation level
--- vim.opt.tabstop = 4           -- Number of spaces a tab counts for
--- vim.opt.softtabstop = 4       -- Number of spaces a tab key inserts
---
---
--- vim.opt.list = true
--- vim.opt.listchars:append("space:.")
---
--- vim.diagnostic.config({
---   virtual_text = true,     -- Show inline diagnostics
---   signs = true,            -- Show signs in the gutter
---   underline = true,        -- Underline the diagnostic text
---   update_in_insert = true, -- Update diagnostics in insert mode
---   severity_sort = true,    -- Sort diagnostics by severity
--- })
