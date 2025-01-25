e3f0a073a6b2b1cd2830f3108115c859b425709d
sefatanam <sefatanam@gmail.com>
2025-01-26 00:37:09 +0600

    remove hot plugins from init.lua

--- init.lua
+++ init.lua
@@ -15,53 +15,10 @@ local lazy_config = require "configs.lazy"
 require "custom.autocmds"
 require "custom.options"
 
--- require("lazy").setup({ { import = "custom.plugins" } }, lazy_config)
 -- load plugins
 require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-  },
-  { "nvchad/volt", },
-  { "nvchad/minty",            cmd = { "Shades", "Huefy" }, },
-  { "nvchad/menu", },
-  { "Bekaboo/deadcolumn.nvim", },
   { import = "plugins" },
-  {
-    'Bekaboo/dropbar.nvim',
-    dependencies = {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      build = 'make'
-    }
-  },
-  {
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    version = "1.2.0",
-    dependencies = {
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons", -- optional dependency
-    },
-    opts = {}
-  },
-  {
-    "luckasRanarison/tailwind-tools.nvim",
-    name = "tailwind-tools",
-    build = ":UpdateRemotePlugins",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-telescope/telescope.nvim", -- optional
-      "neovim/nvim-lspconfig",         -- optional
-    },
-    opts = {}                          -- your configuration
-  },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-  },
-  { 'akinsho/toggleterm.nvim', version = "*", config = true },
+  { import = "custom.hot-plugins" },
   { import = "custom.plugins" }
 }, lazy_config)
 
---
+++ lua/custom/hot-plugins.lua
@@ -0,0 +1,38 @@
+return {
+  { "NvChad/NvChad",           branch = "v2.5",             import = "nvchad.plugins" },
+  { "nvchad/volt", },
+  { "nvchad/minty",            cmd = { "Shades", "Huefy" }, },
+  { "nvchad/menu", },
+  { "Bekaboo/deadcolumn.nvim", },
+  {
+    'Bekaboo/dropbar.nvim',
+    dependencies = {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make'
+    }
+  },
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "1.2.0",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+  },
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- optional
+      "neovim/nvim-lspconfig",         -- optional
+    },
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+  { 'akinsho/toggleterm.nvim', version = "*", config = true },
+}
