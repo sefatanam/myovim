b5da979843fffd692f3e94369baee2487708675c
sefatanam <sefatanam@gmail.com>
2025-01-25 15:09:26 +0600

    plugin: add git blame

--- init.lua
+++ init.lua
@@ -104,9 +104,9 @@ end)
 
 require("telescope").load_extension("ui-select")
 -- Background transparent
-vim.cmd [[
-  highlight Normal guibg=none
-  highlight NonText guibg=none
-  highlight Normal ctermbg=none
-  highlight NonText ctermbg=none
-]]
+-- vim.cmd [[
+--   highlight Normal guibg=none
+--   highlight NonText guibg=none
+--   highlight Normal ctermbg=none
+--   highlight NonText ctermbg=none
+-- ]]
